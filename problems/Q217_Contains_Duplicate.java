29afd4a3fefd82d1303fedbbc54fadc404104828
Devansh Upadhyay <devanshu9719@gmail.com>
2022-01-25 22:25:07 +0530

    Added Q-217: Contains Duplicate

---
+++ problems/Q217_Contains_Duplicate.java
@@ -0,0 +1,37 @@
+/*
+----------------------------------------------------PROBLEM---------------------------------------
+
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+
+Example 1:
+
+	Input: nums = [1,2,3,1]
+	Output: true
+
+Example 2:
+
+	Input: nums = [1,2,3,4]
+	Output: false
+
+Example 3:
+
+	Input: nums = [1,1,1,3,3,4,3,2,4,2]
+	Output: true
+---------------------------------------------------------------------------------------------------
+---------------------------------SOLUTION----------------------------------------------------------
+
+Use a HashSet
+---------------------------------------------------------------------------------------------------
+*/
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> hs = new HashSet<>();
+        for(int x: nums){
+            if(hs.contains(x))
+                return true;
+            hs.add(x);
+        }
+        return false;
+    }
+}
